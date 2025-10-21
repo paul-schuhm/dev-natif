2f9591619dd977bb3bbd3035fcb8285a5888a02a
paul schuhmacher <contact@pschuhmacher.com>
2025-10-21 06:22:30 +0200

    mise a jour

--- demos-c/demo-encapsulation-stricte/car.h
+++ demos-c/demo-encapsulation-stricte/car.h
@@ -1,7 +1,9 @@
 
 //Pointeur opaque stricte.
 typedef void car;
+
+//API
 void set_price(car* c, float price);
-void* object_car_create();
+car* object_car_create();
 void object_car_destroy(car*);
 void object_car_print(car* c);
